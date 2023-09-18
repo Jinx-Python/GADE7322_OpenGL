9e77032645b455e514041a98651e0c1d125dc5f6
DatNinja <cameronterwin@gmail.com>
2023-09-18 14:41:58 +0200

    Added height map generation
    throwing an exception, so its commented out for the time being

--- Game_Engine - GADE7322/Game_Engine/main.cpp
+++ Game_Engine - GADE7322/Game_Engine/main.cpp
@@ -26,6 +26,82 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    int mapWidth, mapHeight, nChannels;
+    unsigned char* mapData = stbi_load("resources/textures/HightMap.png",
+                                        &mapWidth, &mapHeight, &nChannels, 0);
+
+    // vertex generation
+    std::vector<float> vertices;
+    float yScale = 64.0f / 256.0f, yShift = 16.0f;  // apply a scale+shift to the height data
+    for (unsigned int i = 0; i < mapHeight; i++)
+    {
+        for (unsigned int j = 0; j < mapWidth; j++)
+        {
+            // retrieve texel for (i,j) tex coord
+            unsigned char* texel = mapData + (j + mapWidth * i) * nChannels;
+            // raw height at coordinate
+            unsigned char y = texel[0];
+
+            // vertex
+            vertices.push_back(-mapHeight / 2.0f + i);        // v.x
+            vertices.push_back((int)y * yScale - yShift); // v.y
+            vertices.push_back(-mapWidth / 2.0f + j);        // v.z
+        }
+    }
+
+    stbi_image_free(mapData);
+
+    // index generation
+    std::vector<unsigned int> indices;
+    for (unsigned int i = 0; i < mapHeight - 1; i++)       // for each row a.k.a. each strip
+    {
+        for (unsigned int j = 0; j < mapWidth; j++)      // for each column
+        {
+            for (unsigned int k = 0; k < 2; k++)      // for each side of the strip
+            {
+                indices.push_back(j + mapWidth * (i + k));
+            }
+        }
+    }
+
+    const unsigned int NUM_STRIPS = mapHeight - 1;
+    const unsigned int NUM_VERTS_PER_STRIP = mapWidth * 2;
+
+    // register VAO
+    GLuint terrainVAO, terrainVBO, terrainEBO;
+    glGenVertexArrays(1, &terrainVAO);
+    glBindVertexArray(terrainVAO);
+
+    glGenBuffers(1, &terrainVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, terrainVBO);
+    glBufferData(GL_ARRAY_BUFFER,
+        vertices.size() * sizeof(float),       // size of vertices buffer
+        &vertices[0],                          // pointer to first element
+        GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glGenBuffers(1, &terrainEBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, terrainEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+        indices.size() * sizeof(unsigned int), // size of indices buffer
+        &indices[0],                           // pointer to first element
+        GL_STATIC_DRAW);
+
+    // draw mesh
+    glBindVertexArray(terrainVAO);
+    // render the mesh triangle strip by triangle strip - each row at a time
+    for (unsigned int strip = 0; strip < NUM_STRIPS; ++strip)
+    {
+        glDrawElements(GL_TRIANGLE_STRIP,   // primitive type
+            NUM_VERTS_PER_STRIP, // number of indices to render
+            GL_UNSIGNED_INT,     // index data type
+            (void*)(sizeof(unsigned int)
+                * NUM_VERTS_PER_STRIP
+                * strip)); // offset to starting index
+    }
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "MyGameEngine", NULL, NULL);
     if (window == NULL)
@@ -99,7 +175,7 @@ int main()
     //};
 
     //basicMesh myTriangle(vertices);
-    std::vector<basicCubeVertex> vertices = {
+   /* std::vector<basicCubeVertex> vertices = {
         {glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec2(0.0f, 0.0f)},
         {glm::vec3(0.5f, -0.5f, -0.5f), glm::vec2(1.0f, 0.0f)},
         {glm::vec3(0.5f,  0.5f, -0.5f), glm::vec2(1.0f, 1.0f)},
@@ -141,8 +217,8 @@ int main()
         {glm::vec3(0.5f,  0.5f,  0.5f), glm::vec2(1.0f, 0.0f)},
         {glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec2(0.0f, 0.0f)},
         {glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec2(0.0f, 1.0f)},
-    };
-    basicCubeMesh myCube(vertices);
+    };*/
+    //basicCubeMesh myCube(vertices);
 
 
 
@@ -198,7 +274,7 @@ int main()
 
         myShader.setMat4("projection", projection);
         myShader.setMat4("model", model);
-        myCube.Draw(myShader);
+        //myCube.Draw(myShader);
 
         //math
 
