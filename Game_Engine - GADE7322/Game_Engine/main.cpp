4bcc5273e67c4c12c8fe0aa2a035f09ab4e611e3
DatNinja <cameronterwin@gmail.com>
2023-09-18 20:28:47 +0200

    commented out the rotations

--- Game_Engine - GADE7322/Game_Engine/main.cpp
+++ Game_Engine - GADE7322/Game_Engine/main.cpp
@@ -215,8 +215,8 @@ int main()
         //cube 
         glm::mat4 model = glm::mat4(1.0f);
         //model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 1.0f));
+        //model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        //model = glm::translate(model, glm::vec3(0.0f, 0.0f, 1.0f));
 
         glm::mat4 view = glm::mat4(1.0f);
         // note that we're translating the scene in the reverse direction of where we want to move
