69b1991d19aeaf453fe8cd57c3e295e1f28a39eb
DatNinja <cameronterwin@gmail.com>
2023-10-25 23:52:29 +0200

    Update

--- Game_Engine - GADE7322/Game_Engine/main.cpp
+++ Game_Engine - GADE7322/Game_Engine/main.cpp
@@ -268,7 +268,7 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-    
+
 
     //all input managing things
 }
