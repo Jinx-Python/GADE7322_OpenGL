03d459fe836d86130bac193fbeb4d5117aafb68c
Python <nadiadupreez3@gmail.com>
2023-11-17 16:48:40 +0200

    Updated the camera.h file for the free look camera

--- Game_Engine - GADE7322/Game_Engine/Camera.h
+++ Game_Engine - GADE7322/Game_Engine/Camera.h
@@ -13,12 +13,26 @@
 class Camera {
 public:
     Camera();
-    void processKeyboardInput(int key, int action);
+    void processInput(GLFWwindow* window, int key, int action);
     glm::mat4 getViewMatrix();
 
 private:
     glm::vec3 cameraPositions[3];
     int currentCameraPositionIndex;
     glm::mat4 viewMatrix;
+    glm::vec3 cameraPosition;  //Position of the free look camera
+    glm::vec3 cameraFront;     //Front vector of the free look camera
+    glm::vec3 cameraUp;        //Up vector of the free look camera
+    bool freeLook;             //Flag indicating whether the camera is in free look mode
+
+    // Functions for free look camera movement
+    void updateFreeLookCameraVectors();
+    void processFreeLookInput(GLFWwindow* window, int key, int action);
+    void processMouseMovement(GLFWwindow* window, double xpos, double ypos);
+
+    // Constants for camera movement
+    float cameraSpeed;
+    float sensitivity;
 };
+
 #endif
